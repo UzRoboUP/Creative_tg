3782c25b8741dab9d4d721040d17f4d66a9c3baa
ravshankhatamovv <156951199+ravshankhatamovv@users.noreply.github.com>
2024-05-30 11:44:55 +0500

    Language reconfigured

--- create_travel/src/press_service/serializers.py
+++ create_travel/src/press_service/serializers.py
@@ -5,7 +5,7 @@ class NewsSerializer(serializers.ModelSerializer):
     
     class Meta:
         model=models.News
-        fields="__all__"
+        fields=['title', 'content','slug','published_at','id','image','is_active']
         lookup_field = 'slug'
         extra_kwargs = {
             'url': {'lookup_field': 'slug'}
